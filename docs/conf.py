970856fa79e85bbbdf5f850dd92e68a1dc0668c2
thebigmunch <mail@thebigmunch.me>
2018-10-22 18:25:40 -0400

    Use stable version for audio-metadata intsersphinx

--- docs/conf.py
+++ docs/conf.py
@@ -79,7 +79,7 @@ todo_include_todos = True
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
 	'python': ('https://docs.python.org/3', None),
-	'audio_metadata': ('https://audio-metadata.readthedocs.io/en/latest', None),
+	'audio_metadata': ('https://audio-metadata.readthedocs.io/en/stable/', None),
 	'marshmallow': ('https://marshmallow.readthedocs.io/en/latest/', None),
 }
 
